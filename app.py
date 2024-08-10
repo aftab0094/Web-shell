033e1d009fb65c614770cd17bd7d0811b97bcf09
Parrot Live user <user@localhost.localdomain>
2024-08-10 08:09:51 +0000

    twekead the database, index.html deleted shell_output added post method jobs

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
+import subprocess as sp
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
@@ -15,9 +16,34 @@ class shell(db.Model):
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    if request.method =='POST':
+        command_content = request.form['shell_show']
+        new_command = shell(shell_input=command_content)
+
+        try:
+            command_output = sp.check_output(command_content, shell=True, text=True)
+        except sp.CalledProcessError:
+            command_output = 'command not found'
+        except Exception as e:
+            command_output = f'Error: {str(e)}'
+        new_command.shell_output = command_output
+
+        print(f'{command_content}\n{command_output}')
+
+        try:
+            db.session.add(new_command)
+            db.session.commit()
+            return redirect('/')
+        except Exception as e:
+            raise Exception(e)
+    else:
+        commands = shell.query.all()
+        print(commands)
+        for command in commands:
+            print(command)
+            print(command.shell_input)
+            print(command.shell_output)
+        return render_template('index.html', commands=commands)
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
